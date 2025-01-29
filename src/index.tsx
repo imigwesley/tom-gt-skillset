45d4466263693091eda51f527502940c3c1411b5
Wesley Imig <imig.wa03@gmail.com>
2025-01-29 18:14:20 -0500

    configure amplify in root

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-quill/dist/quill.snow.css';
+import { Amplify } from 'aws-amplify';
+import awsmobile from './aws-exports';
 
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
