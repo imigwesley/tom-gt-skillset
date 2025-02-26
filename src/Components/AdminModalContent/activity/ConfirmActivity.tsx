26899f123d9184b26cb695fc960d9a6c314c0cd7
Wesley Imig <imig.wa03@gmail.com>
2025-02-26 17:37:27 -0500

    bug fix: activity image not shown before deletion

--- src/Components/AdminModalContent/activity/ConfirmActivity.tsx
+++ src/Components/AdminModalContent/activity/ConfirmActivity.tsx
@@ -3,10 +3,29 @@ import { ModalPages } from "../../../Types/enums";
 import { AdminModalContentProps } from "../../../Types/props";
 import { isActivityInformation } from "../../../utils/Utils";
 import '../AdminModalContent.scss';
+import { useEffect, useState } from "react";
+import { useImageCache } from "../../../ImageCacheContext";
+
 
 
 const ConfirmActivity = ({saveOrDelete, userInput, tempImage}: AdminModalContentProps) => {
   const localActivityData = isActivityInformation(userInput) ? userInput: undefined;
+  const { getImage } = useImageCache();
+
+  const [localImage, setLocalImage] = useState('')
+
+
+  useEffect(() => {
+    const setImage = async () => {
+      if (tempImage) {
+        setLocalImage(tempImage);
+      } else if (userInput && isActivityInformation(userInput)) {
+        const blobPath = await getImage(userInput.imagePath);
+        setLocalImage(blobPath);
+      }
+    }
+    setImage();
+  }, []);
 
   return (
     <div className='input-info-container'>
@@ -26,7 +45,7 @@ const ConfirmActivity = ({saveOrDelete, userInput, tempImage}: AdminModalContent
         </div>
         <div className='confirm-section'>
           <Typography variant="h6" className='italics'>Activity Thumbnail:</Typography>
-          {tempImage && <img src={tempImage} alt="Selected" style={{ width: '200px', height: '200px', objectFit: 'cover' }} />}
+          {localImage && <img src={localImage} alt="Selected" style={{ width: '200px', height: '200px', objectFit: 'cover' }} />}
         </div>
       </div>
     </div>
